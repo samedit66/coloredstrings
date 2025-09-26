d2cf1d94d12cf051d0be65eddc53cf1fe63c03bf
samedit66 <samedit66@yandex.ru>
2025-09-26 17:51:08 +0300

    refactor(style_builder): make _with_colors, _with_attrs private methods

--- src/coloredstrings/style_builder.py
+++ src/coloredstrings/style_builder.py
@@ -63,15 +63,134 @@ class StyleBuilder:
 
     @property
     def on(self) -> StyleBuilder:
-        """Return a new StyleBuilder where the next color call will set the background."""
         return StyleBuilder(self.fg, self.bg, self.attrs, True)
 
-    def with_attrs(self, *attrs: Attribute) -> StyleBuilder:
+    @property
+    def black(self) -> StyleBuilder:
+        return self._with_color(Color.BLACK)
+
+    @property
+    def red(self) -> StyleBuilder:
+        return self._with_color(Color.RED)
+
+    @property
+    def green(self) -> StyleBuilder:
+        return self._with_color(Color.GREEN)
+
+    @property
+    def yellow(self) -> StyleBuilder:
+        return self._with_color(Color.YELLOW)
+
+    @property
+    def blue(self) -> StyleBuilder:
+        return self._with_color(Color.BLUE)
+
+    @property
+    def magenta(self) -> StyleBuilder:
+        return self._with_color(Color.MAGENTA)
+
+    @property
+    def cyan(self) -> StyleBuilder:
+        return self._with_color(Color.CYAN)
+
+    @property
+    def white(self) -> StyleBuilder:
+        return self._with_color(Color.WHITE)
+
+    @property
+    def bright_black(self) -> StyleBuilder:
+        return self._with_color(Color.BRIGHT_BLACK)
+
+    @property
+    def gray(self) -> StyleBuilder:
+        return self.bright_black
+
+    @property
+    def grey(self) -> StyleBuilder:
+        return self.bright_black
+
+    @property
+    def bright_red(self) -> StyleBuilder:
+        return self._with_color(Color.BRIGHT_RED)
+
+    @property
+    def bright_green(self) -> StyleBuilder:
+        return self._with_color(Color.BRIGHT_GREEN)
+
+    @property
+    def bright_yellow(self) -> StyleBuilder:
+        return self._with_color(Color.BRIGHT_YELLOW)
+
+    @property
+    def bright_blue(self) -> StyleBuilder:
+        return self._with_color(Color.BRIGHT_BLUE)
+
+    @property
+    def bright_magenta(self) -> StyleBuilder:
+        return self._with_color(Color.BRIGHT_MAGENTA)
+
+    @property
+    def bright_cyan(self) -> StyleBuilder:
+        return self._with_color(Color.BRIGHT_CYAN)
+
+    @property
+    def bright_white(self) -> StyleBuilder:
+        return self._with_color(Color.BRIGHT_WHITE)
+
+    def color256(self, index: int) -> StyleBuilder:
+        return self._with_color(Extended256(index=index))
+
+    def rgb(self, r: int, g: int, b: int) -> StyleBuilder:
+        return self._with_color(Rgb(r=r, g=g, b=b))
+
+    def hex(self, color_code: str) -> StyleBuilder:
+        return self._with_color(rgb_from_hex(color_code))
+
+    @property
+    def bold(self) -> StyleBuilder:
+        return self._with_attrs(Attribute.BOLD)
+
+    @property
+    def dim(self) -> StyleBuilder:
+        return self._with_attrs(Attribute.DIM)
+
+    @property
+    def italic(self) -> StyleBuilder:
+        return self._with_attrs(Attribute.ITALIC)
+
+    @property
+    def underline(self) -> StyleBuilder:
+        return self._with_attrs(Attribute.UNDERLINE)
+
+    @property
+    def blink(self) -> StyleBuilder:
+        return self._with_attrs(Attribute.SLOW_BLINK)
+
+    @property
+    def rapid_blink(self) -> StyleBuilder:
+        return self._with_attrs(Attribute.RAPID_BLINK)
+
+    @property
+    def inverse(self) -> StyleBuilder:
+        return self._with_attrs(Attribute.INVERSE)
+
+    @property
+    def hidden(self) -> StyleBuilder:
+        return self._with_attrs(Attribute.HIDDEN)
+
+    @property
+    def strike(self) -> StyleBuilder:
+        return self._with_attrs(Attribute.STRIKE)
+
+    def __repr__(self) -> str:
+        return f"StyleBuilder(fg={self.fg!r}, bg={self.bg!r}, attrs={set(self.attrs)!r}, on={self._on})"
+    
+    def _with_attrs(self, *attrs: Attribute) -> StyleBuilder:
         """Return a new StyleBuilder with given attributes added."""
         new_attrs = self.attrs.union(attrs)
         return StyleBuilder(self.fg, self.bg, new_attrs, self._on)
 
-    def with_color(self, color: typing.Union[Color, Extended256, Rgb]) -> StyleBuilder:
+    def _with_color(self, color: typing.Union[Color, Extended256, Rgb]) -> StyleBuilder:
         """
         Return a new StyleBuilder with color applied.
         If self._on is True, the color will be set to background and _on cleared.
@@ -89,126 +208,6 @@ class StyleBuilder:
             fg = color
 
         return StyleBuilder(fg, bg, self.attrs, on_flag)
-
-    @property
-    def black(self) -> StyleBuilder:
-        return self.with_color(Color.BLACK)
-
-    @property
-    def red(self) -> StyleBuilder:
-        return self.with_color(Color.RED)
-
-    @property
-    def green(self) -> StyleBuilder:
-        return self.with_color(Color.GREEN)
-
-    @property
-    def yellow(self) -> StyleBuilder:
-        return self.with_color(Color.YELLOW)
-
-    @property
-    def blue(self) -> StyleBuilder:
-        return self.with_color(Color.BLUE)
-
-    @property
-    def magenta(self) -> StyleBuilder:
-        return self.with_color(Color.MAGENTA)
-
-    @property
-    def cyan(self) -> StyleBuilder:
-        return self.with_color(Color.CYAN)
-
-    @property
-    def white(self) -> StyleBuilder:
-        return self.with_color(Color.WHITE)
-
-    @property
-    def bright_black(self) -> StyleBuilder:
-        return self.with_color(Color.BRIGHT_BLACK)
-
-    @property
-    def gray(self) -> StyleBuilder:
-        return self.bright_black()
-
-    @property
-    def grey(self) -> StyleBuilder:
-        return self.bright_black()
-
-    @property
-    def bright_red(self) -> StyleBuilder:
-        return self.with_color(Color.BRIGHT_RED)
-
-    @property
-    def bright_green(self) -> StyleBuilder:
-        return self.with_color(Color.BRIGHT_GREEN)
-
-    @property
-    def bright_yellow(self) -> StyleBuilder:
-        return self.with_color(Color.BRIGHT_YELLOW)
-
-    @property
-    def bright_blue(self) -> StyleBuilder:
-        return self.with_color(Color.BRIGHT_BLUE)
-
-    @property
-    def bright_magenta(self) -> StyleBuilder:
-        return self.with_color(Color.BRIGHT_MAGENTA)
-
-    @property
-    def bright_cyan(self) -> StyleBuilder:
-        return self.with_color(Color.BRIGHT_CYAN)
-
-    @property
-    def bright_white(self) -> StyleBuilder:
-        return self.with_color(Color.BRIGHT_WHITE)
-
-    def color256(self, index: int) -> StyleBuilder:
-        return self.with_color(Extended256(index=index))
-
-    def rgb(self, r: int, g: int, b: int) -> StyleBuilder:
-        return self.with_color(Rgb(r=r, g=g, b=b))
-
-    def hex(self, color_code: str) -> StyleBuilder:
-        return self.with_color(rgb_from_hex(color_code))
-
-    @property
-    def bold(self) -> StyleBuilder:
-        return self.with_attrs(Attribute.BOLD)
-
-    @property
-    def dim(self) -> StyleBuilder:
-        return self.with_attrs(Attribute.DIM)
-
-    @property
-    def italic(self) -> StyleBuilder:
-        return self.with_attrs(Attribute.ITALIC)
-
-    @property
-    def underline(self) -> StyleBuilder:
-        return self.with_attrs(Attribute.UNDERLINE)
-
-    @property
-    def blink(self) -> StyleBuilder:
-        return self.with_attrs(Attribute.SLOW_BLINK)
-
-    @property
-    def rapid_blink(self) -> StyleBuilder:
-        return self.with_attrs(Attribute.RAPID_BLINK)
-
-    @property
-    def inverse(self) -> StyleBuilder:
-        return self.with_attrs(Attribute.INVERSE)
-
-    @property
-    def hidden(self) -> StyleBuilder:
-        return self.with_attrs(Attribute.HIDDEN)
-
-    @property
-    def strike(self) -> StyleBuilder:
-        return self.with_attrs(Attribute.STRIKE)
-
-    def __repr__(self) -> str:
-        return f"StyleBuilder(fg={self.fg!r}, bg={self.bg!r}, attrs={set(self.attrs)!r}, on={self._on})"
 
 
 def rgb_from_hex(color_code: str) -> Rgb:
