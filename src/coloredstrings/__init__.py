c4ed553047e7a1443d88a2083be04ae61f88ca27
samedit66 <samedit66@yandex.ru>
2025-10-07 10:37:53 +0300

    feat: export strip_ansi

--- src/coloredstrings/__init__.py
+++ src/coloredstrings/__init__.py
@@ -1,10 +1,12 @@
 from .style_builder import StyleBuilder
 from .types import ColorMode
+from .utils import strip_ansi
 
 
 style = StyleBuilder()
 
 __all__ = [
+    "strip_ansi",
     "style",
     "ColorMode",
     "StyleBuilder",
